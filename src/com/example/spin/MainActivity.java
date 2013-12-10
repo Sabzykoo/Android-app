abe0198551e50f78fdae4a2ea13a0f5692adcf75
Dean <deanbabic@hotmail.com>
2013-12-10 23:48:59 +0100

    Bug corrected
    Ispravljen bug gdje bi se aplikacija rušila prilikom stiskanja na gumb
    Start

--- src/com/example/spin/MainActivity.java
+++ src/com/example/spin/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,14 +15,15 @@ public class MainActivity extends Activity {
 
 	private RadioGroup difficulty;
 	private RadioButton setdifficulty;
-	
+	private Button StartQuiz;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button StartQuiz= (Button) findViewById(R.id.buttonStart);
-		StartQuiz.setOnClickListener(new View.OnClickListener() {
+		difficulty = (RadioGroup) findViewById(R.id.radioDifficulty);
+		StartQuiz= (Button) findViewById(R.id.buttonStart);
+		StartQuiz.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
