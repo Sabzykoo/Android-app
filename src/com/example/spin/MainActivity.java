5f5d8e784ed83a9f5fb42ce3c06e42e30ef59567
Dean Babic <deanbabic@hotmail.com>
2014-01-10 18:22:59 +0100

    Fixed
    let’s see

--- src/com/example/spin/MainActivity.java
+++ src/com/example/spin/MainActivity.java
@@ -34,28 +34,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		myDatabase = new Database(MainActivity.this);
-<<<<<<< HEAD
-		
-		/*	myDatabase.defineTable("Test");	here you can see how to define table
-			SQLitem item = new SQLitem("What is the biggest land by region",
-				"Russia", 1);	here you can see how to define a row
-			MainActivity.this.myDatabase.addItem(item);	here you can see how to insert a row into defined table*/
-				
-		Cursor c = myDatabase.showAllTables();
-		
-		if (c.moveToFirst()) {
-			c.moveToNext();
-			
-			while(!c.isAfterLast()){
-				tables.add(c.getString(0));
-				c.moveToNext();
-			}
-		}
-		
-		if (tables.size() == 0) {
-			tables.add("Download sets");
-		}
-=======
+
 		/*myDatabase.defineTable("Test"); //here you can see how to define table
 		SQLitem item = new SQLitem("What is the biggest land by region", "Russia", 1); //here you can see how to define a row
 		MainActivity.this.myDatabase.addItem(item);*/ //here you can see how to insert a row into defined table
@@ -72,7 +51,6 @@ public class MainActivity extends Activity {
 		        {
 		            tables.add("Download sets");
 		        }
->>>>>>> 281252cd16d2abe74d71567f661cb51daec18e88
 		Spinner spinner = (Spinner) findViewById(R.id.spinnerCategory);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, 
