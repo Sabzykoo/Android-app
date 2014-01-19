5b28115412baafea1f2dc86ceaa7e66509450f22
Dean Babic <deanbabic@hotmail.com>
2014-01-19 12:26:24 +0100

    Podešenja

--- src/com/example/spin/CramFetcher.java
+++ src/com/example/spin/CramFetcher.java
@@ -11,11 +11,16 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnDismissListener;
 
 public class CramFetcher extends ListActivity {
 
@@ -56,98 +61,112 @@ public class CramFetcher extends ListActivity {
         
 	}
 
-	 private class GetContacts extends AsyncTask<Void, Void, Void> {
+	private class GetContacts extends AsyncTask<Void, Void, Void> {
 		 
-	        @Override
-	        protected void onPreExecute() {
-	            super.onPreExecute();
-	            // Showing progress dialog
-	            pDialog = new ProgressDialog(CramFetcher.this);
-	            pDialog.setMessage("Please wait...");
-	            pDialog.setCancelable(false);
-	            pDialog.show();
-	 
-	        }
-	 
-	        @Override
-	        protected Void doInBackground(Void... arg0) {
-	            // Creating service handler class instance
-	            ServiceHandler sh = new ServiceHandler();
-	 
-	            // Making a request to url and getting response
-	            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
-	 
-	            Log.d("Response: ", "> " + jsonStr);
-	 
-	            if (jsonStr != null) {
-	                try {
-	                    JSONObject jsonObj = new JSONObject(jsonStr);
-	                     
-	                    // Getting JSON Array node
-	                    contacts = jsonObj.getJSONArray(TAG_CONTACTS);
-	 
-	                    // looping through All Contacts
-	                    for (int i = 0; i < contacts.length(); i++) {
-	                        JSONObject c = contacts.getJSONObject(i);
-	                         
-	                        String id = c.getString(TAG_ID);
-	                        String name = c.getString(TAG_NAME);
-	                        String email = c.getString(TAG_EMAIL);
-	                        String address = c.getString(TAG_ADDRESS);
-	                        String gender = c.getString(TAG_GENDER);
-	 
-	                        // Phone node is JSON Object
-	                        JSONObject phone = c.getJSONObject(TAG_PHONE);
-	                        String mobile = phone.getString(TAG_PHONE_MOBILE);
-	                        String home = phone.getString(TAG_PHONE_HOME);
-	                        String office = phone.getString(TAG_PHONE_OFFICE);
-	 
-	                        // tmp hashmap for single contact
-	                        HashMap<String, String> contact = new HashMap<String, String>();
-	 
-	                        // adding each child node to HashMap key => value
-	                        contact.put(TAG_ID, id);
-	                        contact.put(TAG_NAME, name);
-	                        contact.put(TAG_EMAIL, email);
-	                        contact.put(TAG_PHONE_MOBILE, mobile);
-	 
-	                        // adding contact to contact list
-	                        contactList.add(contact);
-	                    }
-	                } catch (JSONException e) {
-	                    e.printStackTrace();
-	                }
-	            } else {
-	                Log.e("ServiceHandler", "Couldn't get any data from the url");
-	            }
-	 
-	            return null;
-	        }
-	 
-	        @Override
-	        protected void onPostExecute(Void result) {
-	            super.onPostExecute(result);
-	            // Dismiss the progress dialog
-	            if (pDialog.isShowing())
-	                pDialog.dismiss();
-	            /**
-	             * Updating parsed JSON data into ListView
-	             * */
-	            ListAdapter adapter = new SimpleAdapter(
-	                    CramFetcher.this, contactList,
-	                    R.layout.list_item, new String[] { TAG_NAME, TAG_EMAIL,
-	                            TAG_PHONE_MOBILE }, new int[] { R.id.name,
-	                            R.id.email, R.id.mobile });
-	 
-	            setListAdapter(adapter);
-	        }
-	 }
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.cram, menu);
-		return true;
-	}
+		private boolean isTaskCancelled = false;
 
+        public void cancelTask(){
+             isTaskCancelled = true;
+        }
+        
+        private boolean isTaskCancelled(){
+            return isTaskCancelled;
+        }
+        
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(CramFetcher.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(true);
+            pDialog.show();
+            pDialog.setOnCancelListener(new OnCancelListener(){
+
+            	  public void onCancel(DialogInterface dialog) {
+            	   // TODO Auto-generated method stub
+            		  cancelTask();
+            	   Toast.makeText(CramFetcher.this,
+            	     "ProgressDialog Cancelled!",
+            	     Toast.LENGTH_SHORT).show();
+            	  }});
+        }
+ 
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+ 
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+            if (isTaskCancelled()){
+                return null;
+             }
+            Log.d("Response: ", "> " + jsonStr);
+ 
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                     
+                    // Getting JSON Array node
+                    contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+ 
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                         
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NAME);
+                        String email = c.getString(TAG_EMAIL);
+                        String address = c.getString(TAG_ADDRESS);
+                        String gender = c.getString(TAG_GENDER);
+ 
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        String mobile = phone.getString(TAG_PHONE_MOBILE);
+                        String home = phone.getString(TAG_PHONE_HOME);
+                        String office = phone.getString(TAG_PHONE_OFFICE);
+ 
+                        // tmp hashmap for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+ 
+                        // adding each child node to HashMap key => value
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+                        contact.put(TAG_PHONE_MOBILE, mobile);
+ 
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+ 
+            return null;
+        }
+ 
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    CramFetcher.this, contactList,
+                    R.layout.list_item, new String[] { TAG_NAME, TAG_EMAIL,
+                            TAG_PHONE_MOBILE }, new int[] { R.id.name,
+                            R.id.email, R.id.mobile });
+ 
+            setListAdapter(adapter);
+        }
+ 
+    }
+ 
 }
